aef0b4647cba9a639a4be569e52a81b18b210067
vegarfae <vgf@hotmail.no>
2021-02-21 22:14:50 +0100

    Created testData and client classes, for testing purposes.

---
+++ src/main/java/HospitalClient.java
@@ -0,0 +1,29 @@
+import mappe.del1.hospital.Department;
+import mappe.del1.hospital.Employee;
+import mappe.del1.hospital.Hospital;
+import mappe.del1.hospital.Patient;
+import mappe.del1.hospital.exception.RemoveException;
+
+public class HospitalClient {
+    public static void main(String[] args) {
+        Hospital hospital = new Hospital("Sykehuset");
+        HospitalTestData.fillRegisterWithTestData(hospital);
+
+        Department emergency = hospital.getDepartments().get("Akutten");
+        Employee employee = new Employee("Kristian", "Bergsjø", "21020069696");
+        emergency.addEmployee(employee);
+        try {
+            emergency.remove(employee);
+            System.out.println(employee.getFullName() + " was added to " + emergency.getDepartmentName());
+        } catch (RemoveException e){
+            System.out.println(e.getMessage());
+        }
+
+        Patient patient = new Patient("Marius", "Strengen", "01120096969");
+        try {
+            emergency.remove(patient);
+        } catch (RemoveException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/HospitalTestData.java
@@ -0,0 +1,75 @@
+import mappe.del1.hospital.healthpersonnel.Nurse;
+import mappe.del1.hospital.healthpersonnel.doctor.GeneralPractitioner;
+import mappe.del1.hospital.healthpersonnel.doctor.Surgeon;
+import mappe.del1.hospital.*;
+public final class HospitalTestData {
+    private HospitalTestData() {
+        // not called
+    }
+    /**
+     * Fills a given hospital with test data.
+     * @param hospital Name of hospital to fill with data.
+     */
+    public static void fillRegisterWithTestData(final Hospital hospital) {
+        // Add some departments
+        Department emergency = new Department("Akutten");
+
+        Employee employee = new Employee("Odd Even", "Primtallet", "1");
+        emergency.getEmployees().put(employee.getFullName(), employee);
+
+        employee = new Employee("Huppasahn", "DelFinito", "2");
+        emergency.getEmployees().put(employee.getFullName(), employee);
+
+        employee = new Employee("Rigmor", "Mortis", "3");
+        emergency.getEmployees().put(employee.getFullName(), employee);
+
+        Surgeon surgeon = new Surgeon("Inco", "Gnito", "4");
+        emergency.getEmployees().put(surgeon.getFullName(), surgeon);
+
+        Nurse nurse = new Nurse("Nina", "Teknologi", "5");
+        emergency.getEmployees().put(nurse.getFullName(), nurse);
+
+        nurse = new Nurse("Ove", "Ralt", "6");
+        emergency.getEmployees().put(nurse.getFullName(), nurse);
+
+        Patient patient = new Patient("Inga", "Lykke", "7");
+        emergency.getPatients().put(patient.getFullName(), patient);
+
+        patient = new Patient("Ulrik", "Smål", "8");
+        emergency.getPatients().put(patient.getFullName(), patient);
+
+        hospital.getDepartments().put(emergency.getDepartmentName(), emergency);
+
+
+        Department childrenPolyclinic = new Department("Barn poliklinikk");
+
+        employee = new Employee("Salti", "Kaffen", "9");
+        childrenPolyclinic.getEmployees().put(employee.getFullName(), employee);
+
+        employee = new Employee("Nidel V.", "Elvefølger", "10");
+        childrenPolyclinic.getEmployees().put(employee.getFullName(), employee);
+
+        employee = new Employee("Anton", "Nym", "11");
+        childrenPolyclinic.getEmployees().put(employee.getFullName(), employee);
+
+        GeneralPractitioner generalPractitioner = new GeneralPractitioner("Gene", "Sis", "12");
+        childrenPolyclinic.getEmployees().put(generalPractitioner.getFullName(), generalPractitioner);
+
+        surgeon = new Surgeon("Nanna", "Na", "13");
+        childrenPolyclinic.getEmployees().put(surgeon.getFullName(), surgeon);
+
+        nurse = new Nurse("Nora", "Toriet", "14");
+        childrenPolyclinic.getEmployees().put(nurse.getFullName(), nurse);
+
+        patient = new Patient("Hans", "Omvar", "15");
+        childrenPolyclinic.getPatients().put(patient.getFullName(), patient);
+
+        patient = new Patient("Laila", "La", "16");
+        childrenPolyclinic.getPatients().put(patient.getFullName(), patient);
+
+        patient = new Patient("Jøran", "Drebli", "17");
+        childrenPolyclinic.getPatients().put(patient.getFullName(), patient);
+
+        hospital.getDepartments().put(childrenPolyclinic.getDepartmentName(), childrenPolyclinic);
+    }
+}
