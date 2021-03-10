a33cd8171852dc59025b9080fd859636902b4d32
Vegard Færgestad <vegarfae@stud.ntnu.no>
2021-03-11 00:41:53 +0100

    Added a new way to create a Patient test object, now that Patient() is protected.
    Added comments.

--- src/test/java/RemoveTest.java
+++ src/test/java/RemoveTest.java
@@ -5,24 +5,50 @@ import mappe.del1.hospital.healthpersonnel.Nurse;
 import mappe.del1.hospital.Patient;
 import mappe.del1.hospital.healthpersonnel.doctor.Surgeon;
 import org.junit.jupiter.api.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for testing the remove method in Department.
+ */
 public class RemoveTest {
     static Department department;
     static Surgeon surgeon;
     static Patient patient;
     static Nurse nurse;
+    static Constructor<Patient> pat_constructor;
+    static Class<Patient> pat = mappe.del1.hospital.Patient.class;
 
+    /**
+     * Adding test objects.
+     *
+     * @throws NoSuchMethodException     the no such method exception
+     * @throws IllegalAccessException    the illegal access exception
+     * @throws InvocationTargetException the invocation target exception
+     * @throws InstantiationException    the instantiation exception
+     */
     @BeforeAll
-    static void addObjects(){
+    static void addObjects() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // The following code is required to create a patient object to test with, since
+        // the patient constructor is set to protected.
+        pat_constructor = pat.getDeclaredConstructor(String.class, String.class, String.class);
+        pat_constructor.setAccessible(true);
+        patient = pat_constructor.newInstance("Marius", "Strengen", "01120096969");
+
+
         department = new Department("testDepartment");
         surgeon = new Surgeon("Kristian", "Bergsjø", "21020069696");
-        patient = new Patient("Marius", "Strengen", "01120096969");
         nurse = new Nurse("Herman", "Erlandsen", "123456789");
         department.addEmployee(surgeon);
         department.addPatient(patient);
     }
 
+    /**
+     * Test that tries to remove existing employee from the department.
+     */
     @Test
     void removeExistingEmployee(){
         assertTrue(department.getEmployees().containsKey(surgeon.getFullNavn()));
@@ -34,6 +60,9 @@ public class RemoveTest {
         assertFalse(department.getEmployees().containsKey(surgeon.getFullNavn()));
     }
 
+    /**
+     * Test that tries to remove existing patient from the department.
+     */
     @Test
     void removeExistingPatient(){
         assertTrue(department.getPatients().containsKey(patient.getFullNavn()));
@@ -45,6 +74,9 @@ public class RemoveTest {
         assertFalse(department.getPatients().containsKey(patient.getFullNavn()));
     }
 
+    /**
+     * Test that tries to remove not existing employee from the department.
+     */
     @Test
     void removeNotExistingEmployee(){
         GeneralPractitioner generalPractitioner = new GeneralPractitioner("Vegard",
@@ -52,9 +84,12 @@ public class RemoveTest {
         assertThrows(RemoveException.class, () -> department.remove(generalPractitioner));
     }
 
+    /**
+     * Test that tries to remove not existing patient from the department.
+     */
     @Test
-    void removeNotExistingPatient(){
-        Patient patient = new Patient("Kajsa", "Kirkhus", "987654321");
+    void removeNotExistingPatient() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        Patient patient = pat_constructor.newInstance("Kajsa", "Kirkhus", "987654321");
         assertThrows(RemoveException.class, () -> department.remove(patient));
     }
 }
